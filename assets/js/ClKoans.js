23aeea9287fd93747d5cf983250405f6270b0c11
fidelity-ofc <filmistry@artlover.com>
2026-05-21 10:26:32 -0500

    KoanKorner

---
+++ assets/js/ClKoans.js
@@ -0,0 +1,15 @@
+
+
+function GetKoan()
+{
+    var koans = new Array("Knock Knock. <br> Who's there? <br> Who. <br> Who who? <br> I didn't know you.", "Why did the traffic light turn red? <br> You would too.", "What did Ben Franklin say when he flew a kite in a storm? <br> He didn't say anything.", "How do billboards talk? <br> They use language.", "How do angels greet people?", "What goes up and never comes down? <br> You.", "Why was the boy sitting on his watch?<br> He wanted to be.", "How do you make a fire with two sticks? <br> You make one.", "Which hand is it better to write with? <br> Neither hand.", "Why did the kid like learning math? <br> He thought it was real.","What do two oceans do when they meet? <br> Nothing.", "First snake: Are we poinsonous? <br> Second snake: I don't know.", "What's harder to catch the faster you run? <br> You.");
+    var random = koans[Math.floor(Math.random() * koans.length)];
+    document.getElementById("message").innerHTML=random;
+}
+
+function GetClown()
+{
+    var clowns = new Array("face1", "face2", "face3");
+    var clowndom = clowns[Math.floor(Math.random() * clowns.length)];
+    document.getElementById("clown").src="/images/" + clowndom + ".png";
+}
